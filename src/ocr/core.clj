88da977ddffa3f63d71392defe97b1d730050eea
Dan Sutton <danielsutton01@gmail.com>
2017-06-16 15:29:13 -0500

    Final parsing product

--- src/ocr/core.clj
+++ src/ocr/core.clj
@@ -1,3 +1,32 @@
 (ns ocr.core
   (:require [clojure.string :as str]
-            [ocr.parse :as p]))
+            [ocr.parse :as p]
+            [ocr.files :as f]))
+
+(defn format-account
+  [digits]
+  (str/join "" (map #(if (#{:not-recognized} %) "?" (str %)) digits)))
+
+(defn format-reason
+  [reason]
+  (case reason
+    :not-valid-checksum "ERR"
+    :un-recognized-digits "ILL"
+    :valid ""))
+
+
+(defn parse-file
+  "Given a filename that will resolve to a correctly formatted
+  file (three lines of ascii and a blank line separating from the
+  next), print the the parsed account and parsing message.
+
+  If you need to create a file matching this, use the `ocr.files`
+  namespace for file creation and reading."
+  [filename]
+  (let [pairs (->> (f/account-numbers-streams filename)
+                   (map p/parse)
+                   (map (juxt identity p/analyze-parse)))]
+    (doseq [[account reason] pairs]
+      (println (format "%s %s"
+                       (format-account account)
+                       (format-reason reason))))))
